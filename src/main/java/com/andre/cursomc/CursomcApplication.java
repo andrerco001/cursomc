05109f0b88252b116ecfc708e22c3060be2c2270
Andre Oliveira <andreoliveira.adv@hotmail.com>
2018-09-16 15:19:03 -0400

    Creating Instance Operation

--- src/main/java/com/andre/cursomc/CursomcApplication.java
+++ src/main/java/com/andre/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.andre.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.andre.cursomc.domain.Category;
+import com.andre.cursomc.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class CursomcApplication 
+public class CursomcApplication implements CommandLineRunner 
 {
+	@Autowired
+	CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) 
 	{
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception 
+	{
+		Category cat1 = new Category(null, "Computing");
+		Category cat2 = new Category(null, "Office");
+		
+		categoryRepository.save(Arrays.asList(cat1, cat2));
+		
+		
 	}
 }
